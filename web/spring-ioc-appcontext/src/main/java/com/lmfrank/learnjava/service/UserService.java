a352842d5a4f13d674ba415dcbf406ba24817785
xsc <njuxushuchi@163.com>
2022-02-22 14:45:27 +0800

    update

---
+++ web/spring-ioc-appcontext/src/main/java/com/lmfrank/learnjava/service/UserService.java
@@ -0,0 +1,40 @@
+package com.lmfrank.learnjava.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+
+    private MailService mailService;
+
+    public void setMailService(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    private List<User> users = new ArrayList<>(List.of(
+            new User(1, "bob@example.com", "password", "Bob"),
+            new User(2, "alice@example.com", "password", "Alice"),
+            new User(3, "tom@example.com", "password", "Tome")));
+
+    public User login(String email, String password) {
+        for (User user : users) {
+            if (user.getEmail().equalsIgnoreCase(email) && user.getPassword().equals(password)) {
+                mailService.sendLoginMail(user);
+                return user;
+            }
+        }
+        throw new RuntimeException("login failed.");
+    }
+
+    public User register(String email, String password, String name) {
+        users.forEach((user) -> {
+            if (user.getEmail().equalsIgnoreCase(email)) {
+                throw new RuntimeException("email exist.");
+            }
+        });
+        User user = new User(users.stream().mapToLong(User::getId).max().getAsLong(), email, password, name);
+        users.add(user);
+        mailService.sendRegistrationMail(user);
+        return user;
+    }
+}
