9e98ae63359fbb1437aeccf778d7c68c911e81d1
LMFrank <njuxushuchi@163.com>
2023-03-15 14:21:27 +0800

    update

--- 多线程/ch01/src/Main.java
+++ 多线程/ch01/src/Main.java
@@ -29,9 +29,31 @@ class MyRunnable implements Runnable {
 }*/
 
 
+/*
 public class Main {
     public static void main(String[] args) {
         Thread t = new Thread(() -> System.out.println("start new thread"));
         t.start();
+    }
+}*/
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main start...");
+        Thread t = new Thread(() -> {
+            System.out.println("thread run...");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                System.out.println("thread end.");
+            }
+        });
+        t.start();
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            System.out.println("main end...");
+        }
     }
 }
---
+++ 多线程/ch02/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(() -> System.out.println("hello"));
+        System.out.println("start");
+        t.start();
+        t.join();
+        System.out.println("end");
+    }
+}
